2cb99c3fbe466c3be60f9c51f2dcba89fd409030
nikitarec4nik <105701254+nikitarec4nik@users.noreply.github.com>
2022-06-09 13:55:35 +0300

    .

---
+++ практика 23/1/1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button button2 = new Button();
+            button2.Top = button1.Top + button1.Height;
+            button2.Left = button1.Left;
+            this.Controls.Add(button2);
+        }
+    }
+}
---
+++ практика 23/1/3/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Pen pen = new Pen(Color.Black, 2);
+        private void Form1_paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+            ///клавиатура
+            g.DrawLine(pen, 250, 250, 250, 260);
+            g.DrawLine(pen, 250, 260, 350, 260);
+            g.DrawLine(pen, 350, 260, 350, 250);
+            g.DrawLine(pen, 350, 250, 250, 250);
+            ///монитор
+            g.DrawLine(pen, 270, 230, 270, 170);
+            g.DrawLine(pen, 270, 170, 330, 170);
+            g.DrawLine(pen, 330, 170, 330, 230);
+            g.DrawLine(pen, 330, 230, 270, 230);
+            ///ножка
+            g.DrawLine(pen, 295, 230, 295, 250);
+            g.DrawLine(pen, 305, 230, 305, 250);
+            ///системник
+            g.DrawLine(pen, 380, 170, 380, 260);
+            g.DrawLine(pen, 380, 260, 420, 260);
+            g.DrawLine(pen, 420, 260, 420, 170);
+            g.DrawLine(pen, 420, 170, 380, 170);
+        }
+    }
+}
