00eeb42b55d3d47cae4073151643754d177bc31a
nikitarec4nik <105701254+nikitarec4nik@users.noreply.github.com>
2022-06-09 13:57:24 +0300

    .

---
+++ практика 22/практика 22/задание 1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace z5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = 0.1722;
+            double y = 6.33;
+            double z = 3.25*Math.Pow(10, -4);
+            double f = 5*Math.Atan(x)-(1/4)*Math.Acos(x)*(x+3*Math.Abs(x-y)+Math.Pow(x, 2))/(Math.Abs(x-y)*z+Math.Pow(x,2));
+            textBox3.Text = f.ToString();
+        }
+    }
+}
---
+++ практика 22/практика 22/задание 2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace z6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double es = 2.72, s = 0 ;
+            double x = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            double f = Math.Pow(x,2);
+            if ((1 < x * b) & (x * b < 10))
+            {
+                s = Math.Pow(es, f);
+            }
+            else if ((12 < f) & (f < 40))
+            {
+                s = Math.Sqrt(Math.Abs(f + 4 * b));
+            }
+            else s = b * Math.Pow(f,2);
+            
+            textBox4.Text = Convert.ToString(s);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
