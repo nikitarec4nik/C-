94ce246beba69419c3209bbe593c8eb6474ca55b
nikitarec4nik <105701254+nikitarec4nik@users.noreply.github.com>
2022-06-16 11:46:08 +0300

    .

---
+++ практика 29/задание 1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace задание_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStartTimer = new System.Windows.Forms.Button();
+            this.btnStopTimer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStartTimer
+            // 
+            this.btnStartTimer.Location = new System.Drawing.Point(313, 413);
+            this.btnStartTimer.Name = "btnStartTimer";
+            this.btnStartTimer.Size = new System.Drawing.Size(75, 23);
+            this.btnStartTimer.TabIndex = 0;
+            this.btnStartTimer.Text = "StartTimer";
+            this.btnStartTimer.UseVisualStyleBackColor = true;
+            // 
+            // btnStopTimer
+            // 
+            this.btnStopTimer.Location = new System.Drawing.Point(430, 413);
+            this.btnStopTimer.Name = "btnStopTimer";
+            this.btnStopTimer.Size = new System.Drawing.Size(75, 23);
+            this.btnStopTimer.TabIndex = 1;
+            this.btnStopTimer.Text = "Strop Timer";
+            this.btnStopTimer.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStopTimer);
+            this.Controls.Add(this.btnStartTimer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStartTimer;
+        private System.Windows.Forms.Button btnStopTimer;
+    }
+}
+
---
+++ практика 29/задание 1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace задание_1
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        private Pen pen;
+        public Form1()
+        {
+            InitializeComponent();
+
+            pen=new Pen(Color.DarkRed, 2);
+
+            this.Load += Form1_Load;
+
+            timer1.Tick += Timer1_Tick;
+            btnStartTimer.Click += BtnStartTimer_Click;
+            btnStopTimer.Click += BtnStopTimer_Click;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            timer1.Interval = 1000;
+
+            r = 150;
+            a = 0;
+            x1 = this.ClientSize.Width / 2;
+            y1 = this.ClientSize.Height / 2;
+            x2 = x1 + (int)(r * Math.Cos(a));
+            y2 = y1 - (int)(r * Math.Sin(a));
+        }
+
+        private void BtnStopTimer_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+        private void BtnStartTimer_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Start();
+        }
+        private int x1, x2, y1, y2, r;        
+
+        private double a;     
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            g.DrawLine(pen, x1, y1, x2, y2);
+        }
+        private void Timer1_Tick(object sender, EventArgs e)
+        {            
+            a -= 0.1;
+            x2 = x1 + (int)(r*Math.Cos(a));
+            y2 = (y1 - (int)(r*Math.Sin(a)));           
+            Invalidate();
+        }
+    }
+}
